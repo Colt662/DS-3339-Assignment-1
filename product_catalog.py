b83ce89037e8169a52faa242981f3b6f44fe7005
Colt662 <141377849+Colt662@users.noreply.github.com>
2026-09-12 12:49:09 -0500

    Update product_catalog.py

--- product_catalog.py
+++ product_catalog.py
@@ -1,31 +1,25 @@
 from product_data import products
-# TODO: Step 1 - Print out the products to see the data that you are working with.
 
-
-
-# TODO: Step 2 - Create a list called customer_preferences and store the user preference in this list.
-
-
+#Create a customer_preferences list and store the user preference in this list.
+customer_preferences = []
 response = ""
 while response != "N":
     print("Input a preference:")
     preference = input()
     # Add the customer preference to the list
+    customer_preferences.append(preference)
 
     response = input("Do you want to add another preference? (Y/N): ").upper()
-  
-
-# TODO: Step 3 - Convert customer_preferences list to set to eliminate duplicates.
 
 
+# Convert customer preferences and product tag lists to set to eliminate duplicates and allow for fast comparisons.
+customer_preferences = set(customer_preferences)
 
-# TODO: Step 4 - Convert the product tags to sets in order to allow for faster comparisons.
-converted_products = []
+for product in products:
+    product["tags"] = set(product["tags"])
 
 
-
-
-# TODO: Step 5 - Write a function to calculate the number of matching tags
+# Function to calculate the number of matching tags for a product
 def count_matches(product_tags, customer_tags):
     '''
     Args:
@@ -34,29 +28,52 @@ def count_matches(product_tags, customer_tags):
     Returns:
         int: The number of matching tags between the product and customer.
     '''
-    pass
+    return len(customer_tags.intersection(product_tags))
 
 
-
-
-# TODO: Step 6 - Write a function that loops over all products and returns a sorted list of matches
+#Function that loops over all products and returns a sorted list of matches
 def recommend_products(products, customer_tags):
     '''
     Args:
         products (list): A list of product dictionaries.
         customer_tags (set): A set of tags associated with the customer.
     Returns:
-        list: A list of products containing product names and their match counts.
+        list: A sorted list of products containing product names and their match counts.
     '''
-    pass
+    matches = []
+
+    for product in products:
+        num_matches = count_matches(product["tags"],customer_tags)
+        if num_matches > 0:
+            matches.append({"product": product["name"],"matches": num_matches})
+    #sorted in descending order by number of matches
+    matches.sort(reverse=True, key=lambda x:x["matches"])
+    return matches
+
+
+#Get the recommendations and print the results
+matched_products = recommend_products(products, customer_preferences)
+
+print("Recommended Products:")
+for match in matched_products:
+    print(f"- {match["product"]:30} {match["matches"]} match(es)")
 
 
 
-# TODO: Step 7 - Call your function and print the results
-
-
-
-
-# DESIGN MEMO (write below in a comment):
+# DESIGN MEMO (write below in a comment): 200-300 words
 # 1. What core operations did you use (e.g., intersections, loops)? Why?
 # 2. How might this code change if you had 1000+ products?
+
+# 1. What core operations did you use (e.g., intersections, loops)? Why?
+# A while loop was used to input user preferences so that any amount of tags may be entered.
+# For-in loops were used to access the products list so that the whole list could be accessed.
+# An intersection was used to count product tag matches because it is a fast way to compare.
+# Both the user preferred tags and the product tags are converted to and kept as sets since for the purposes of recommendations it isn't necessary to have duplicate or ordered tags, and they would both likely be used primarily for comparisons even in other situations.
+
+#2. How might this code change if you had 1000+ products?
+# If there were 1000+ products the underlying logic wouldn't need much if any change, since the for-in loops work for any sized list, but additional effort would be needed to make the output more useful to the user, since there would be more products recommended for a given number of preferred tags.
+# There would likely be many products that would have the same number of matches, with many having all their tags matching user preferences, so additional sorting of those top products would be desired, as well either a possible culling of the bottom options after the first ~100 or so matches, or a GUI which only show a certain amount of matches per page.
+# A possible solution to apply additional sorting to the output would be by inputting user priority of preferred tags. E.G. the user would prefer to see products that have the "lightweight" tag over ones that have the "durable" tag, so for products that have the same number of matches, those with the "lightweight" tag would be higher up in the list.
+# If the list of products was long enough to run into the limitations of storing or accessing lists in Python (I am unsure of how large that would be), they might need to be stored in such a way that would necessitate additional logic to properly import and convert the list.
+# In such a circumstance the list might even need to be compared in chunks of a certain amount of products at a time which would require additional logic to make sure the results were only shown once all comparisons were made.
+# If the list of products were even longer such that comparing the tags of all products was infeasible, the products would have to be stored in such a way that only a fraction of them had to be compared to the users preferences, which I would have to research more into, perhaps using hash tables
